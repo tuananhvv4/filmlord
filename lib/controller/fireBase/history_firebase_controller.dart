c4e6391fc759ce8fbee4c524fec24166c672991c
tuananhvv4 <anh2k2st@gmail.com>
2024-10-19 21:23:14 +0700

    init project

--- lib/controller/fireBase/history_firebase_controller.dart
+++ lib/controller/fireBase/history_firebase_controller.dart
@@ -4,40 +4,40 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class HistoryFirebaseController {
-
   final FirebaseFirestore db = FirebaseFirestore.instance;
 
   final String collectionName = 'history';
   final String childCollection = 'episodes';
 
-
-  String getCurrentEmail(){
+  String getCurrentEmail() {
     return FirebaseAuth.instance.currentUser!.email.toString();
   }
 
-  Stream<QuerySnapshot> historyQuery(){
-    final querySnapshot = db.collection('users')
+  Stream<QuerySnapshot> historyQuery() {
+    final querySnapshot = db
+        .collection('users')
         .doc(getCurrentEmail())
         .collection('history')
-        .orderBy('time',descending: true)
+        .orderBy('time', descending: true)
         .limit(10)
         .snapshots();
     return querySnapshot;
   }
 
-  Stream<QuerySnapshot> episodeQuery(String documentID){
-    final querySnapshot = db.collection('users')
+  Stream<QuerySnapshot> episodeQuery(String documentID) {
+    final querySnapshot = db
+        .collection('users')
         .doc(getCurrentEmail())
         .collection('history')
         .doc(documentID)
         .collection(childCollection)
-        .orderBy('time',descending: true)
+        .orderBy('time', descending: true)
         .limit(1)
         .snapshots();
     return querySnapshot;
   }
 
-  Future<bool> checkDocumentExists( String documentId) async {
+  Future<bool> checkDocumentExists(String documentId) async {
     try {
       // Truy cập vào tài liệu dựa trên ID trong Firestore
       DocumentSnapshot documentSnapshot = await db
@@ -55,7 +55,8 @@ class HistoryFirebaseController {
     }
   }
 
-  Future<bool> checkEpisodeExists( String documentId, String childDocumentID) async {
+  Future<bool> checkEpisodeExists(
+      String documentId, String childDocumentID) async {
     try {
       // Truy cập vào tài liệu dựa trên ID trong Firestore
       DocumentSnapshot documentSnapshot = await db
@@ -75,78 +76,80 @@ class HistoryFirebaseController {
     }
   }
 
-  Future addHistoryMovie(String name, String slug, String thumb_url, String poster_url) async {
-
+  Future addHistoryMovie(
+      String name, String slug, String thumb_url, String poster_url) async {
     bool existDocument = await checkDocumentExists(slug);
 
-    final DocumentReference documentReference = db.collection('users')
+    final DocumentReference documentReference = db
+        .collection('users')
         .doc(getCurrentEmail())
         .collection('history')
         .doc(slug);
-    if(existDocument){
-      documentReference.update({
-        'time' : Timestamp.now()
-      });
-    }else{
+    if (existDocument) {
+      documentReference.update({'time': Timestamp.now()});
+    } else {
       try {
-        documentReference.set(
-            {
-              'name': name,
-              'slug': slug,
-              'thumb_url': thumb_url,
-              'poster_url': poster_url,
-              'time': Timestamp.now()
-            }
-        );
-      } catch (e){
+        documentReference.set({
+          'name': name,
+          'slug': slug,
+          'thumb_url': thumb_url,
+          'poster_url': poster_url,
+          'time': Timestamp.now()
+        });
+      } catch (e) {
         log(e.toString());
       }
     }
   }
 
-  Future addEpisode(String movieName, String slug, String thumb_url, String poster_url, String movie_url, String episodeName,  int index) async {
+  Future addEpisode(
+      String movieName,
+      String slug,
+      String thumb_url,
+      String poster_url,
+      String movie_url,
+      String episodeName,
+      int index) async {
+    addHistoryMovie(movieName, slug, thumb_url, poster_url);
 
-    addHistoryMovie(movieName,slug,thumb_url,poster_url);
+    bool existEpisode = await checkEpisodeExists(slug, episodeName);
 
-    bool existEpisode = await checkEpisodeExists(slug,episodeName);
-
-    final DocumentReference documentReference = db.collection('users')
+    final DocumentReference documentReference = db
+        .collection('users')
         .doc(getCurrentEmail())
         .collection('history')
         .doc(slug)
         .collection(childCollection)
         .doc(episodeName);
-    if(existEpisode){
-      documentReference.update({
-        'time' : Timestamp.now()
-      });
-    }else{
+    if (existEpisode) {
+      documentReference.update({'time': Timestamp.now()});
+    } else {
       try {
-        documentReference.set(
-            {
-              'episodeName': episodeName,
-              'slug': slug,
-              'movie_url': movie_url,
-              'index' : index,
-              'currentWatchingTime' : 0,
-              'time': Timestamp.now()
-            }
-        );
-      } catch (e){
+        documentReference.set({
+          'episodeName': episodeName,
+          'slug': slug,
+          'movie_url': movie_url,
+          'index': index,
+          'currentWatchingTime': 0,
+          'time': Timestamp.now()
+        });
+      } catch (e) {
         log(e.toString());
       }
     }
   }
 
-  Future<void> updateWatchingTime( String slug, String episodeName, int currentWatchingTime) async {
-    final DocumentReference documentReference = db.collection('users')
+  Future<void> updateWatchingTime(
+      String slug, String episodeName, int currentWatchingTime) async {
+    final DocumentReference documentReference = db
+        .collection('users')
         .doc(getCurrentEmail())
         .collection('history')
         .doc(slug)
         .collection(childCollection)
         .doc(episodeName);
     documentReference.update({
-      'currentWatchingTime' : currentWatchingTime,
+      'currentWatchingTime': currentWatchingTime,
     });
   }
 
@@ -154,16 +157,17 @@ class HistoryFirebaseController {
     try {
       // Kiểm tra xem currentUserEmail có null không trước khi truy cập thuộc tính email
       if (getCurrentEmail().isNotEmpty) {
-        final DocumentReference documentReference = db.collection('users')
+        final DocumentReference documentReference = db
+            .collection('users')
             .doc(getCurrentEmail())
             .collection(collectionName)
             .doc(docID);
-        final CollectionReference collectionReference = db.collection('users')
+        final CollectionReference collectionReference = db
+            .collection('users')
             .doc(getCurrentEmail())
             .collection(collectionName)
             .doc(docID)
             .collection('episodes');
-
 
         await documentReference.delete();
       } else {
@@ -174,11 +178,11 @@ class HistoryFirebaseController {
     }
   }
 
-
   Future<void> deleteDocumentWithSubCollections(String docID) async {
     try {
       // Lấy tài liệu cần xóa
-      final documentRef = db.collection('users')
+      final documentRef = db
+          .collection('users')
           .doc(getCurrentEmail())
           .collection(collectionName)
           .doc(docID);
@@ -202,31 +206,28 @@ class HistoryFirebaseController {
     }
   }
 
-
-
   Future addHistoryActivity(String name, String slug, String action) async {
     String actionContent = '';
-    if(action == 'comment'){
+    if (action == 'comment') {
       actionContent = 'Đã thêm 1 bình luận!';
     }
-    if(action == 'rating'){
+    if (action == 'rating') {
       actionContent = 'Đã thêm 1 đánh giá!';
     }
-    final DocumentReference documentReference = db.collection('users')
+    final DocumentReference documentReference = db
+        .collection('users')
         .doc(getCurrentEmail())
         .collection('activity')
         .doc();
     try {
-      documentReference.set(
-          {
-            'movie': name,
-            'slug': slug,
-            'action' : action,
-            'content' : actionContent,
-            'time': Timestamp.now()
-          }
-      );
-    } catch (e){
+      documentReference.set({
+        'movie': name,
+        'slug': slug,
+        'action': action,
+        'content': actionContent,
+        'time': Timestamp.now()
+      });
+    } catch (e) {
       log(e.toString());
     }
   }
